d5387674543195a2b67746b55108ed56cc49a5a9
prabk123 <prabodhkakodkar@gmail.com>
2022-01-13 19:40:14 +0000

    apply new code

--- index.js
+++ index.js
@@ -1,56 +1,32 @@
 const puppeteer = require('puppeteer');
+const { stringify } = require('querystring');
 
-(async function() {
+async function scrape(i) {
+    console.log('starting...')
+    console.log(i)
+    let base_url = "https://rarity.tools/mypethooligan/view/"
+    let rank_url = base_url+i
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://rarity.tools/mypethooligan/view/1599", { waitUntil: 'networkidle0' });
+    
+    await page.goto(rank_url, { waitUntil: 'networkidle0' });
 
     const result = await page.evaluate(async () => {
         console.log('Browser scope.');
-        let elementTxtArr = [];
+        let elementTxtArr = null;
         document.querySelectorAll("span.font-bold.whitespace-nowrap").forEach((a)=> {
            console.log(a.innerText);
-           elementTxtArr.push(a.innerText);
+           elementTxtArr = a.innerText;
         });
         return elementTxtArr;
     });
-    console.log(result);
+    console.log(i,result);
 
     await browser.close();
-})();
+    return(i,result)
+};
 
-
-
-// const { strictEqual } = require('assert');
-// const puppeteer = require('puppeteer');
-// const { stringify } = require('querystring');
-// let nft_ranks = []
-// const browser = await puppeteer.launch();
-// const page = await browser.newPage();
-
-// function scrapingFunc(rank) {
-
-//     let base_url = "https://rarity.tools/mypethooligan/view/"
-//     let rank_url = base_url+stringify(rank)
-//     await page.goto(rank_url, { waitUntil: 'networkidle0' });
-
-//     const result = await page.evaluate(async () => {
-//         console.log('Browser scope.');
-//         let elementTxtArr = [];
-//         document.querySelectorAll("span.font-bold.whitespace-nowrap").forEach((a)=> {
-//            console.log(a.innerText);
-//            elementTxtArr.push(a.innerText);
-//         });
-//         return elementTxtArr;
-//     });
-//     console.log(result);
-    
-//     await browser.close();
-//     return(result)
-// }
-
-
-// for (let i =0; i <= 10; i++) {
-//     let scraped_rank = scrapingFunc(i)
-//     console.log("ID:",i,"Rank:", scraped_rank)
-//  }
+for (let i =0; i <= 7; i++) {
+    results= scrape(i)
+    console.log(results)
+ }
